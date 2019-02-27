28290a485c0d3ecac91ba595924207970ddb42e0
山下　鷹徳 <tyamashita@grandream.jp>
2019-02-27 20:19:49 +0900

    動的ルーティングコンテンツの出し分け (http://localhost:3000/users/{qiita}) {qiita}部分が可変する

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <p>
-      /users/_id.vue
+      User ID: {{userID}}
     </p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userID: this.$route.params.id
+    }
+  }
+}
+</script>
