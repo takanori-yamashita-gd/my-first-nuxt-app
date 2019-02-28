73ea29e2bc1b93a310b17fb633287c48dd8f5b86
山下　鷹徳 <tyamashita@grandream.jp>
2019-02-28 20:36:18 +0900

    console.logを削除

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,5 @@
 export default function({ $axios }) {
   $axios.onRequest((config) => {
-    console.log(process.env.QIITA_TOKEN);
     if (process.env.QIITA_TOKEN) {
       config.headers.common['Authorization'] = `Bearer ${process.env.QIITA_TOKEN}`;
     }
